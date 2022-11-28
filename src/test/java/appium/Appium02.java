34c2301c2a67aa0ffcd6daa0662fb8ad68c9e7d8
rjumamuradov <rozymyrat.jumamuradov@gmail.com>
2022-11-28 23:48:36 +0100

    appium03

---
+++ src/test/java/appium/Appium02.java
@@ -0,0 +1,15 @@
+package appium;
+
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class Appium02 {
+
+
+    @Test
+     public void test01(){
+        DesiredCapabilities desiredCapabilities=new DesiredCapabilities();
+
+
+    }
+}
---
+++ src/test/java/appium/Appium03.java
@@ -0,0 +1,13 @@
+package appium;
+
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class Appium03 {
+    @Test
+    public void appium03(){
+        DesiredCapabilities desiredCapabilities=new DesiredCapabilities();
+
+
+    }
+}
